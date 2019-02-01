84fd19bddb190d04f3ac7f3ce203034f4dc0ab78
Kevin Huang <kev.huang28@gmail.com>
2019-01-31 23:28:44 -0800

    Add example models

--- backend/gql/admin.py
+++ backend/gql/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from gql.models import Book, Subject, Student
+
+
+admin.site.register(Book)
+admin.site.register(Subject)
+admin.site.register(Student)
--- backend/gql/models.py
+++ backend/gql/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    subject = models.ForeignKey(
+        'Subject',
+        on_delete=models.CASCADE
+    )
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    books = models.ManyToManyField(Book)
+    subjects = models.ManyToManyField(Subject)
