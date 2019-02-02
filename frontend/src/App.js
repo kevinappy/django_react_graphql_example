c7ec51964b5dd14b0975ae3773dd54d4fed929c7
Kevin Huang <kev.huang28@gmail.com>
2019-02-02 15:48:12 -0800

    Move query out to its own constant

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,52 +5,54 @@ import environment from "./RelayEnvironment";
 
 const graphql = require("babel-plugin-relay/macro");
 
+const AppQuery = graphql`
+  query AppQuery {
+    allStudents(first: 5) {
+      totalCount
+      edges {
+        node {
+          id
+          firstName
+          lastName
+          age
+          books {
+            edges {
+              node {
+                id
+                subject {
+                  id
+                  name
+                  code
+                }
+              }
+            }
+          }
+          subjects {
+            edges {
+              node {
+                id
+                name
+                code
+              }
+            }
+          }
+        }
+      }
+      pageInfo {
+        hasNextPage
+        startCursor
+        endCursor
+      }
+    }
+  }
+`;
+
 class App extends Component {
   render() {
     return (
       <QueryRenderer
         environment={environment}
-        query={graphql`
-          query AppQuery {
-            allStudents(first: 5) {
-              totalCount
-              edges {
-                node {
-                  id
-                  firstName
-                  lastName
-                  age
-                  books {
-                    edges {
-                      node {
-                        id
-                        subject {
-                          id
-                          name
-                          code
-                        }
-                      }
-                    }
-                  }
-                  subjects {
-                    edges {
-                      node {
-                        id
-                        name
-                        code
-                      }
-                    }
-                  }
-                }
-              }
-              pageInfo {
-                hasNextPage
-                startCursor
-                endCursor
-              }
-            }
-          }
-        `}
+        query={AppQuery}
         variables={{}}
         render={({ error, props }) => {
           if (error) {
@@ -59,7 +61,6 @@ class App extends Component {
           if (!props) {
             return <div>Loading...</div>;
           }
-          console.log(props);
           let elements = [];
           props.allStudents.edges.forEach(student => {
             let books = [];
